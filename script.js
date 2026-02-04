63f322b73196a85830e459b45db28a55f088bfd7
bkkclasses <namanj.data@gmail.com>
2026-02-04 11:04:14 +0530

    Update and rename homepage.js to script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const menuItems = document.querySelectorAll("#menu li");
+const menuItems = document.querySelectorAll(".menu li");
 const pages = document.querySelectorAll(".page");
 
 menuItems.forEach(item => {
   item.addEventListener("click", () => {
-    // Remove active class
     menuItems.forEach(i => i.classList.remove("active"));
     pages.forEach(p => p.classList.remove("active"));
 
-    // Add active class
     item.classList.add("active");
-    const sectionId = item.getAttribute("data-section");
-    document.getElementById(sectionId).classList.add("active");
+    document.getElementById(item.dataset.page).classList.add("active");
   });
 });
